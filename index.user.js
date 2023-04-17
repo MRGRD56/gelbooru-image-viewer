2cc00411630b3092f85c63fb1622b82ebd0b7047
k.golikov <kgolikov@37bytes.com>
2023-04-18 01:51:10 +0500

    Some fixes

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         gelbooru quick image viewer
 // @namespace    https://github.com/MRGRD56
-// @version      0.1
+// @version      0.2
 // @author       MRGRD56
 // @match        https://gelbooru.com/index.php
 // @match        https://gelbooru.com/index.php*
@@ -20,15 +20,76 @@
         return;
     }
 
-    const getFullImageUrlSample = (inputURL) => {
-        return inputURL.replace(/thumbnails\/(\w+)\/(\w+)\/thumbnail_(\w+)\.jpg/, 'samples/$1/$2/sample_$3.jpg');
-    }
+    const getFullImageUrlSample = (inputUrl) => {
+        return inputUrl.replace(/thumbnails\/(\w+)\/(\w+)\/thumbnail_(\w+)\.jpg/, 'samples/$1/$2/sample_$3.jpg');
+    };
 
-    const getFullImageUrlImage = (inputURL) => {
-        return inputURL.replace(/thumbnails\/(\w+)\/(\w+)\/thumbnail_(\w+)\.jpg/, 'images/$1/$2/$3.jpg');
-    }
+    const getFullImageUrlImage = (inputUrl) => {
+        return inputUrl.replace(/thumbnails\/(\w+)\/(\w+)\/thumbnail_(\w+)\.jpg/, 'images/$1/$2/$3.jpg');
+    };
+
+    const replaceExtension = (extension) => {
+        return {
+            'jpg': 'jpeg',
+            'jpeg': 'png',
+            'png': 'webp'
+        }[extension] || extension;
+    };
+
+    const replaceFullImageUrlExtension = (inputUrl) => {
+        return inputUrl.replace(/\.(\w{2,})$/, (match, extension) => '.' + replaceExtension(extension));
+    };
 
     const postListPosts = document.querySelector('main .thumbnail-container');
+
+    const viewHandler = (e, o) => {
+        console.log('VIEW', e, o);
+
+        const originalImage = e.detail.originalImage;
+        const viewedImage = e.detail.image;
+
+        const errorHandler = () => {
+            viewedImage.removeEventListener('load', loadHandler);
+            viewedImage.removeEventListener('error', errorHandler);
+
+            originalImage.removeAttribute('data-image-loaded');
+
+            if (originalImage.getAttribute('data-is-full-image') === 'true') {
+                const oldLink = originalImage.src;
+                const newLink = replaceFullImageUrlExtension(originalImage.getAttribute('data-full'));
+
+                if (newLink === oldLink) {
+                    return;
+                }
+
+                originalImage.setAttribute('data-full', newLink);
+                viewedImage.src = newLink;
+            } else {
+                const oldLink = originalImage.src;
+                const newLink = getFullImageUrlImage(originalImage.src);
+
+                if (newLink === oldLink) {
+                    return;
+                }
+
+                originalImage.setAttribute('data-full', newLink);
+                originalImage.setAttribute('data-is-full-image', 'true');
+                viewedImage.src = newLink;
+            }
+
+            viewHandler(e, o);
+        };
+
+        const loadHandler = () => {
+            viewedImage.removeEventListener('load', loadHandler);
+            viewedImage.removeEventListener('error', errorHandler);
+
+            originalImage.setAttribute('data-image-loaded', 'true');
+        };
+
+        viewedImage.addEventListener('error', errorHandler);
+        viewedImage.addEventListener('load', loadHandler);
+    };
 
     const viewer = new Viewer(postListPosts, {
         url(image) {
@@ -46,27 +107,9 @@
         },
         transition: false,
         view(e, o) {
-            console.log('VIEW', e, o);
-
-            const originalImage = e.detail.originalImage;
-            const viewedImage = e.detail.image;
-
-            const errorHandler = () => {
-                viewedImage.removeEventListener('load', loadHandler);
-                viewedImage.removeEventListener('error', errorHandler);
-
-                const newLink = getFullImageUrlImage(originalImage.src);
-                originalImage.setAttribute('data-full', newLink)
-                viewedImage.src = newLink;
-            };
-
-            const loadHandler = () => {
-                viewedImage.removeEventListener('load', loadHandler);
-                viewedImage.removeEventListener('error', errorHandler);
-            };
-
-            viewedImage.addEventListener('error', errorHandler);
-            viewedImage.addEventListener('load', loadHandler);
+            if (e.detail.originalImage.getAttribute('data-image-loaded') !== 'true') {
+                viewHandler(e, o);
+            }
         }
     });
 
